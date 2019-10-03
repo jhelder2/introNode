147d4935bc6b4bebe7b8775ae1321b89b7fa5956
Jeffrey Helder <jeffrey.d.helder@gmail.com>
2019-10-03 12:22:06 -0400

    f

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-const express = require('express');
-const port = process.env.PORT || 3000;
+// const express = require('express');
+// const hbs = require('hbs');
+// const path = require('path');
+// const port = process.env.PORT || 3000;
 
-const app = express();
+// const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-})
+// app.use(express.static('public'));
 
-app.listen(port, () =>
-console.log(`app is running on port ${port}`);
-};
+// app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname + "views"));
 
-// const http = require('http');
+// app.get('/', (req, res) => {
+//   res.render('home', {homemessage:"hey there", bio: "anything"});
+// })
 
-// const hostname = '127.0.0.1';//local host
-// const port = process.env.PORT || 3000; //this is a node convention
+// app.listen(port, () => {
+// console.log(`app is running on port ${port}`);
+// })
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
+const http = require('http');
 
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
+const hostname = '127.0.0.1';//local host
+const port = process.env.PORT || 3000; //this is a node convention
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
