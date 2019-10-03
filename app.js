b2be0146d6a2f2a715df2fd53ec4771f01e63a61
Jeffrey Helder <jeffrey.d.helder@gmail.com>
2019-10-03 11:20:08 -0400

    ?

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const http = require('http');
+const express = require('express');
+const port = process.env.PORT || 3000;
 
-const hostname = '127.0.0.1';//local host
-const port = process.env.PORT || 3000; //this is a node convention
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.get('/', (req, res) => {
+  res.send('hello world');
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () =>
+console.log(`app is running on port ${port}`);
+};
+
+// const http = require('http');
+
+// const hostname = '127.0.0.1';//local host
+// const port = process.env.PORT || 3000; //this is a node convention
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
